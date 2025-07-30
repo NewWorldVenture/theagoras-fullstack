2eee4387053cdaca2501db60e713f86ed0b2e40c
NewWorldVenture <newworldventurellc@gmail.com>
2025-07-30 19:23:24 -0400

    Add files via upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Browse from './pages/Browse'
-import Dashboard from './pages/Dashboard'
-import CreateListing from './pages/CreateListing'
-import Profile from './pages/Profile'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to TheAgoras Marketplace</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/browse" element={<Browse />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/create-listing" element={<CreateListing />} />
-      <Route path="/profile/:id" element={<Profile />} />
-    </Routes>
-  </BrowserRouter>
-)
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
